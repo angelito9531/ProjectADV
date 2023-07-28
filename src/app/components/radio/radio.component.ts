7e3fd5b0e1797cd3193c53ac37cdb9976a55129c
angelito9531 <anghelo.rodriap@gmail.com>
2023-07-28 10:13:53 -0400

    button play

--- src/app/components/radio/radio.component.ts
+++ src/app/components/radio/radio.component.ts
@@ -16,23 +16,17 @@ export class RadioComponent implements AfterViewInit, OnInit {
   volumen:number = 0.5;
   constructor() { }
   ngOnInit(): void {
-    this.btnelement = document.getElementById('audio')   
+   
     this.getdt.asObservable().subscribe((data:HTMLAudioElement) =>{
 
     })
   }
   ngAfterViewInit() {
-    this.btnelement.play()
-    this.btnRadio = false;
-    console.log("load",this.audioref.nativeElement.onload) 
-    this.getdt.next(this.audioref.nativeElement);
-    //this.playRadio()
-    
     // let poops = setInterval(()=>{
     //   if(this.audioref.nativeElement){
     //       console.log("BTN PLAY") 
     //       this.playRadio()
-    //       clearInterval(poops);                    
+    //       clearInterval(poops);
     //   }
     // }, 1000);
     // this.audioref.nativeElement.addEventListener('canplay',(ev)=>{
@@ -42,8 +36,7 @@ export class RadioComponent implements AfterViewInit, OnInit {
   }
   playRadio(){    
     this.btnRadio = false; 
-    const radio = this.audioref.nativeElement
-    radio.muted = false;
+    const radio = this.audioref.nativeElement   
     radio.play()
    
   }
